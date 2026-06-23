e8b4f6e1cd74394e1a3e4831326557f3109fa2d8
RealRyanJames <realcoder2023@gmail.com>
2026-06-23 19:46:56 -0400

    ADDED MONGODB CODE

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use mongodb::{bson::doc, options::ClientOptions};
+use std::error::Error;
+
 use actix_web::{App, HttpResponse, HttpServer, Responder, get};
 
 #[get("/")]
@@ -5,10 +8,41 @@ async fn get_home() -> impl Responder {
     HttpResponse::Ok().body("Hello World".to_uppercase())
 }
 
+async fn get_client(data: String) -> Result<(), Box<dyn Error>> {
+    let option = ClientOptions::parse("mongodb://localhost:27017").await?;
+    let client = mongodb::Client::with_options(option)?;
+
+    let db = client.database("data");
+    let coll = db.collection("states_info");
+    coll.insert_one(doc! {"data": data}).await?;
+
+    Ok(())
+}
+
 struct State {
     cap: String,
     state: String,
     pop: f32,
+}
+
+#[get("/Population/Maine")]
+async fn get_maine_pop() -> impl Responder {
+    let maine_pop_state = State {
+        cap: "Augusta".to_string(),
+        state: String::from("Maine"),
+        pop: 1.40000 as f32,
+    };
+
+    HttpResponse::Ok().body(format!(
+        "Population: {} | Capital: {} | State: {} ",
+        maine_pop_state.pop, maine_pop_state.cap, maine_pop_state.state
+    ));
+
+    get_client(format!(
+        "Population: {} | Capital: {} | State: {} ",
+        maine_pop_state.pop, maine_pop_state.cap, maine_pop_state.state
+    ))
+    .await
 }
 
 #[get("/Population/MA")]
@@ -22,7 +56,13 @@ async fn get_pop() -> impl Responder {
     HttpResponse::Ok().body(format!(
         "Population: {} | Capital: {} | State: {} ",
         ma_state.pop, ma_state.cap, ma_state.state
+    ));
+
+    get_client(format!(
+        "Population: {} | Capital: {} | State: {} ",
+        ma_state.pop, ma_state.cap, ma_state.state
     ))
+    .await
 }
 
 #[get("/Population/Califonia")]
@@ -36,7 +76,13 @@ async fn get_pop_cali() -> impl Responder {
     HttpResponse::Ok().body(format!(
         "Population: {} | Capital: {} | State: {} ",
         cali_state.pop, cali_state.cap, cali_state.state
+    ));
+
+    get_client(format!(
+        "Population: {} | Capital: {} | State: {} ",
+        cali_state.pop, cali_state.cap, cali_state.state
     ))
+    .await
 }
 
 #[actix_web::main]
@@ -46,6 +92,7 @@ async fn main() -> std::io::Result<()> {
             .service(get_home)
             .service(get_pop)
             .service(get_pop_cali)
+            .service(get_maine_pop)
     })
     .bind(("127.0.0.1", 3000))?
     .run()
